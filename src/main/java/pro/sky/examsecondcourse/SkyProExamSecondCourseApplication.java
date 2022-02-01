b3ae6a2771a90c1d8b85613accd4629685e06b19
Сергей <040620@mail.ru>
2022-02-01 18:16:32 +0300

    Initial commit

---
+++ src/main/java/pro/sky/examsecondcourse/SkyProExamSecondCourseApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.examsecondcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProExamSecondCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProExamSecondCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/examsecondcourse/SkyProExamSecondCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.examsecondcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProExamSecondCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
